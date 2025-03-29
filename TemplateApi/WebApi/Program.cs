b9d0883273764473fa01444f2bef41a384b5e8cd
Phelipe Noleto <48934827+pnoleto@users.noreply.github.com>
2025-03-29 19:18:11 -0300

    fix: startup in a minimal format

--- TemplateApi/WebApi/Program.cs
+++ TemplateApi/WebApi/Program.cs
@@ -1,59 +1,51 @@
 using Infra.DI;
 using System.Reflection;
 
-namespace WebApi
-{
-    static class Program
-    {
-        private static void Main(string[] args)
-        {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            builder.AddConfigurationItems();
-            builder.AddOpenTelemetryLogger();
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer()
-                 .AddSwaggerDefinitions(Assembly.GetExecutingAssembly().GetName().Name)
-                 //.ExecuteMigrationsOnStartup(builder.Configuration, "NewsConnection")
-                 .AddSqlServerDbContext(builder.Configuration, "NewsConnection")
-                 .AddHttpCLientFactory(builder.Configuration)
-                 .AddHangFireSchedulerWithInMemoryDb()
-                 .AddOpenTelemetryInstrumentation()
-                 .AddExceptionHandler()
-                 .AddCorsDefinitions()
-                 .AddJwtDefinitions()
-                 .AddScheduledJobs()
-                 .AddHealthCheckUI()
-                 .AddRepositories()
-                 .AddHttpClient()
-                 .AddFeedRobots()
-                 .AddMediator()
-                 .AddLogging();
+builder.AddConfigurationItems();
+builder.AddOpenTelemetryLogger();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer()
+     .AddSwaggerDefinitions(Assembly.GetExecutingAssembly().GetName().Name)
+     //.ExecuteMigrationsOnStartup(builder.Configuration, "NewsConnection")
+     .AddSqlServerDbContext(builder.Configuration, "NewsConnection")
+     .AddHttpCLientFactory(builder.Configuration)
+     .AddHangFireSchedulerWithInMemoryDb()
+     .AddOpenTelemetryInstrumentation()
+     .AddExceptionHandler()
+     .AddCorsDefinitions()
+     .AddJwtDefinitions()
+     .AddScheduledJobs()
+     .AddHealthCheckUI()
+     .AddRepositories()
+     .AddHttpClient()
+     .AddFeedRobots()
+     .AddMediator()
+     .AddLogging();
 
-            builder.Services.AddHealthChecks()
-                .CheckSqlServer(builder.Configuration, "NewsConnection")
-                .CheckSystem();
+builder.Services.AddHealthChecks()
+            .CheckSqlServer(builder.Configuration, "NewsConnection")
+            .CheckSystem();
 
-            WebApplication app = builder.Build();
+WebApplication app = builder.Build();
 
-            app.UseRouting()
-                .UseAuthentication()
-                .UseAuthorization()
-                .UseHealthCheckEndpoint();
+app.UseRouting()
+            .UseAuthentication()
+            .UseAuthorization()
+            .UseHealthCheckEndpoint();
 
-            if (app.Environment.IsDevelopment())
-                app.UseHealthChecksUI()
-                    .UseSwaggerDefinitions();
+if (app.Environment.IsDevelopment())
+    app.UseHealthChecksUI()
+        .UseSwaggerDefinitions();
 
-            app.UseProtectedHangFireDashboard()
-                .UseExceptionHandler()
-                .UseHttpsRedirection()
-                .UseScheduledJobs()
-                .UseCors()
-                .UseHsts();
+app.UseProtectedHangFireDashboard()
+            .UseExceptionHandler()
+            .UseHttpsRedirection()
+            .UseScheduledJobs()
+            .UseCors()
+            .UseHsts();
 
-            app.MapControllers();
-            app.Run();
-        }
-    }
-}
+app.MapControllers();
+
+await app.RunAsync();
